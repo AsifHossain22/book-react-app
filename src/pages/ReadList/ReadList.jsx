ffa40b6f1866a52c7329e47ba811fa77fac40f28
AsifHossain22 <only1asif4you22@gmail.com>
2025-10-05 23:50:14 +0400

    Implement sort functionality

--- src/pages/ReadList/ReadList.jsx
+++ src/pages/ReadList/ReadList.jsx
@@ -7,9 +7,10 @@ import Book from "../Book/Book";
 
 const ReadList = () => {
   const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
 
   const data = useLoaderData();
-  console.log(data);
+  //   console.log(data);
 
   useEffect(() => {
     const storedBookData = getStoredBook();
@@ -25,9 +26,36 @@ const ReadList = () => {
     setReadList(myReadList);
   }, []);
 
+  const handleSort = (type) => {
+    setSort(type);
+
+    if (type === "pages") {
+      const sortedByPage = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages
+      );
+      setReadList(sortedByPage);
+      console.log(sortedByPage);
+    } else if (type === "ratings") {
+      const sortedByRatings = [...readList].sort((a, b) => a.rating - b.rating);
+      setReadList(sortedByRatings);
+    }
+  };
+
   return (
     <div>
       <h2>Read List</h2>
+
+      <details className="dropdown">
+        <summary className="btn m-1">sort by : {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("ratings")}>ratings</a>
+          </li>
+        </ul>
+      </details>
 
       <Tabs>
         <TabList>
